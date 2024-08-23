ce39a52338879dca4ea43638c29fca3457b5efb2
turpal88 <pereslav88@gmail.com>
2024-08-23 10:03:42 +0300

    Lesson2/Task1

---
+++ Lesson2/Task1/source/main.cpp
@@ -0,0 +1,38 @@
+#include <QCoreApplication>
+#include <QtNetwork>
+#include <QtSql>
+#include <QDebug>
+
+
+
+
+int main(int argc, char** argv){
+
+    QCoreApplication a(argc, argv);
+    QTcpSocket tcp_socket(QObject *parent = nullptr);
+    QTcpServer tcp_server(QObject *parent = nullptr);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+    db.setDatabaseName("qt_db");
+    db.setHostName("localhost");
+    db.setPort(5432);
+    db.setUserName("postgres");
+    db.setPassword("Cnjkbwf1988!");
+    db.open();
+    if(db.isOpen()){
+        QSqlQuery query;
+        QString str = "CREATE TABLE addressbook (number INTEGER PRIMARY KEY NOT NULL, "
+                      "name VARCHAR (15), phone VARCHAR(12), "
+                      "email VARCHAR(15);";
+        if (!query.exec(str)) {
+            qDebug() << "Unable to create a table";
+        }
+
+    }else{
+
+        qDebug() << "Could not to connect to database";
+    }
+
+
+    return a.exec();
+}
